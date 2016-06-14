32fd64033e847eae5c3ffe4bcbd0b25c85d04edc
Marissa Siegel and Shelby Clayton <marissa@>
2016-06-13 20:03:54 -0700

    initialize favorites website without array information complete

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function(event) {
+var Favorites = ["input#blank2", "input#blank1", "input#blank3"];
+$("#blanks form").submit(function(){
+  $Favorites("#blank2, #blank1, #blank3")
+
+});
+
+});
